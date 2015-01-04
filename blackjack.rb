e4fea6c2e5c9b784cba076d5a4928556836347d4
Rick Peyton <peytorb@gmail.com>
2015-01-04 07:37:46 -0600

    Nearly complete blackjack game. Need to validate input

--- blackjack.rb
+++ blackjack.rb
@@ -2,67 +2,142 @@
 # Tealeaf Academy C01L01
 require 'pry'
 
-Build 2 decks of cards
+def initialize_deck
+  number_of_decks = 2
+  deck = {}
+  decks = []
+  suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  cards =
+    { 'Ace' => 11, 'Two' => 2, 'Three' => 3, 'Four' => 4, 'Five' => 5,
+      'Six' => 6, 'Seven' => 7, 'Eight' => 8, 'Nine' => 9, 'Ten' => 10,
+      'Jack' => 10, 'Queen' => 10, 'King' => 10 }
+  suits.each do |suit|
+    cards.each do |card, value|
+      deck["#{card} of #{suit}"] = value
+    end
+  end
+  number_of_decks.times do
+    deck.each { |card, value| decks << {card => value} }
+  end
+  decks.shuffle
+end
 
-Ask for player's name
+def deal_face_up(cards, player = "Dealer")
+  puts "#{player} is dealt #{cards.last.keys[0]}"
+end
 
-Start game loop
-  shuffle 2 decks of cards into a game deck
+def hand_total(cards)
+  total = 0
+  cards.each { |card| total += card.values[0] }
+  total
+end
 
-  # Bet
-  Ask player to place bet
+puts "What is your name?"
+player = gets.chomp
+# Ask for Chips to Start with
+puts "How many chips would you like to buy?"
+chips = gets.chomp.to_f
+starting_chips = chips
 
-  # Deal
-  Deal the player's first card face down
-  Then deal dealer card face down
-  Deal second card facing up to player
-  deal second card facing up to dealer
+begin
+  game_deck = initialize_deck
 
-  # Insurance
-  if dealer face card is an ace
-    Offer insurance to player
-    (insurance is half the original bet)
+  puts "Place your wager, #{player} (10 - #{chips.to_i})"
+  wager = gets.chomp.to_i
+  chips -= wager
+
+  player_hand = []
+  player_hand << game_deck.pop
+  deal_face_up(player_hand, player)
+
+  dealer_hand = []
+  dealer_hand << game_deck.pop
+  puts "Dealer is dealt a face down card"
+  player_hand << game_deck.pop
+  deal_face_up(player_hand, player)
+
+  dealer_hand << game_deck.pop
+  deal_face_up(dealer_hand)
+
+  if dealer_hand[1].values[0] == 11
+    puts "The Dealer is showing an Ace"
+    puts "Would you like to purchase insurance for #{wager / 2} chips? (y/n)"
+    insurance = gets.chomp.downcase
+    chips -= (wager / 2) if insurance == 'y'
   end
 
-  # Player plays hand
-
-  Begin Player Loop
-    Ask player to stand or hit
-    if hit
-      deal a new face up card
+  begin # Player Plays Hand
+    player_total = hand_total(player_hand)
+    puts "Your current card total is #{player_total}"
+    case
+    when player_total == 21
+      puts "Blackjack!"
+      hit_or_stay = 's'
+    when player_total < 21
+      puts "Would you like to hit or stay? (h/s)"
+      hit_or_stay = gets.chomp.downcase
+      if hit_or_stay == 'h'
+        player_hand << game_deck.pop
+        deal_face_up(player_hand, player)
+      end
+    when player_total > 21
+      puts "Bust!"
+      hit_or_stay = 's'
     end
+  end while hit_or_stay == 'h'
 
-    if the player busts
-      collect their bet
+  puts "Dealer flips first card"
+  puts dealer_hand[0].keys[0]
+  begin # Dealer Plays Hand
+    dealer_total = hand_total(dealer_hand)
+    puts "Dealer current card total is #{dealer_total}"
+    case
+    when dealer_total == 21
+      puts "Blackjack!"
+    when dealer_total < 17
+      dealer_hand << game_deck.pop
+      deal_face_up(dealer_hand)
+    when dealer_total > 21
+      puts "Bust!"
+    when (dealer_total >= 17) && (dealer_total <= 21)
+      puts "Dealer stays at #{dealer_total}"
     end
-  End Player Loop
+  end while dealer_total < 17
 
-  # Dealer Plays Hand
-  Flip dealer's first card
-
-  if dealer has blackjack
-    and player does not have insurance
-    and player does not have blackjack
-      dealer takes bet
-  else player also has blackjack
-    they get their bet back
-  else
-    player keeps bet
+  case
+  when (dealer_total == 21) &&
+       (dealer_hand.length == 2) &&
+       (insurance == 'y')
+    puts "At least you had insurance."
+    puts "#{player} receives his bet back"
+    chips += wager
+  when (player_total == dealer_total) && (player_total <= 21)
+    puts "Draw. #{player} receives his bet back."
+    chips += wager
+  when player_total > 21
+    puts "Sorry, #{player}. You lost your bet."
+  when (player_total < dealer_total) && (dealer_total > 21)
+    puts "You win!"
+    chips += (wager * 2)
+  when player_total < dealer_total
+    puts "Sorry, #{player}. The dealer won. You lost your bet."
+  when (player_total == 21) && (player_hand.length == 2)
+    puts "Blackjack bonus!"
+    puts "#{player} wins #{wager * 1.5} chips!"
+    chips += (wager * 1.5 + wager)
+  when player_total > dealer_total
+    puts "You win!"
+    chips += (wager * 2)
   end
-  
-  Dealer plays their hand last
-  dealer should count ace as 11 until bust then count as 1
-  if the dealer busts then any non-bust player wins
 
-  once dealer stands
-  if player > dealer
-    pay player
-    if they have blackjack on their first
-      hand pay a 3:2 bonus
-    end
-  else if dealer > player
-    collect bet
-  else if tie
-    give back bet
-  end
-End Game Loop
+  puts "Do you want to play another hand? (y/n)"
+  play_again = gets.chomp.downcase
+end while play_again == 'y'
+
+if starting_chips < chips
+  puts "#{player} walks away having won #{(chips - starting_chips).to_i} chips."
+elsif starting_chips > chips
+  puts "#{player} walks away having lost #{(starting_chips - chips).to_i} chips."
+else
+  puts "#{player} walks away with nothing gained, nothing lost."
+end
